96a6aebe219c26baa017cda06a973a30aefab9c0
Arpit Jana <arpitjana2103@gmail.com>
2025-04-23 11:29:43 +0530

    Learned Array Methods

--- JS-Fundamentals/05_array.js
+++ JS-Fundamentals/05_array.js
@@ -1,12 +1,18 @@
 // Array :
 // An array is a collection of similar data elements stored in contiguous memory locations
 
-let arr = [3, 4, 8, 2];
+// let arr = [3, 4, 8, 2];
 // 0 --- arr.length-1
 
-let index = 0;
+// let index = 0;
 
-while (index < arr.length) {
-  console.log(arr[index]);
-  index++;
-}
+// while (index < arr.length) {
+//   console.log(arr[ind]);
+//   index++;
+// }
+
+// Creating array using literal notaion
+let arr = [1, 2, 3];
+
+// Creating array using constructor
+let arr2 = new Array();
---
+++ JS-Fundamentals/06_array_methods.js
@@ -0,0 +1,91 @@
+/*
+let arr = [1, 5, 9];
+
+///////////////////////////////////////////
+////////////////////// Array.prototype.at();
+
+// console.log(arr[1]); // 5
+// console.log(arr.at(1)); // 5
+
+///////////////////////////////////////////
+/////////////////// Array.prototype.push();
+
+arr.push(10);
+arr.push(12);
+
+// console.log(arr);
+
+///////////////////////////////////////////
+//////////////////// Array.prototype.pop();
+
+console.log(arr); // [1,5,9,10,12]
+// const poppedEle = arr.pop();
+console.log(poppedEle); // 12
+
+// console.log(arr); // [1,5,9,10]
+
+///////////////////////////////////////////////
+///////////////////// Array.prototype.concat();
+
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+
+console.log(array3);
+
+
+///////////////////////////////////////////////
+//////////////////////// Array.prototype.fill();
+
+const arr = new Array(10);
+arr.fill("DDUGKY");
+
+let i = 0;
+while (i < arr.length) {
+  console.log(arr.at(i));
+  i++;
+}
+
+
+///////////////////////////////////////////////
+//////////////////////// Array.prototype.indexOf();
+
+let arr = [4, 8, 10, 14, 12];
+console.log(arr.indexOf(10));
+console.log(arr.includes(12));
+
+
+///////////////////////////////////////////////
+//////////////////////// Array.prototype.shift();
+let arr = [1, 2, 3];
+arr.shift();
+
+console.log(arr);
+
+
+/////////////////////////////////////////////////
+//////////////////////// Array.prototype.forEach();
+
+let arr = [5, 8, 25, 10];
+
+// let i = 0;
+// while (i < arr.length) {
+//   let num = arr.at(i);
+//   console.log(num);
+//   i++;
+// }
+
+// arr.forEach(function (num) {
+//   console.log(num);
+// });
+
+/////////////////////////////////////////////////
+//////////////////////// Array.prototype.slice();
+
+const animals = ["ant", "bison", "camel", "duck", "elephant"];
+const newAnimals = animals.slice();
+// const newAnimals = animals.slice(0, 5);
+// start = 0
+// end = arr.length;
+console.log(newAnimals);
+*/
