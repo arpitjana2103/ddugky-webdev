8f85832aa7c4e4d5ee1604696cccc2ae58c375f5
Arpit Jana <arpitjana2103@gmail.com>
2025-05-19 11:26:11 +0530

    q5 done

--- temp2.js
+++ temp2.js
@@ -1,2 +1,21 @@
-const a = NaN;
-console.log(isNaN(a));
+const arr = ["a", "b", "c", "a", "b"];
+
+let obj01 = {};
+
+let i = 0;
+while (i < arr.length) {
+    const key = arr[i];
+    const val = null;
+
+    obj01[key] = val;
+    i++;
+}
+
+let obj02 = Object.keys(
+    arr.reduce(function (acc, ele) {
+        acc[ele] = null;
+        return acc;
+    }, {})
+);
+
+console.log(obj01, obj02);
