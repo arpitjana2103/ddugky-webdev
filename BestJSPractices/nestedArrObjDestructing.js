980d6b765f9fc90f71b6a2c912d2be6293e55756
Arpit Jana <arpitjana2103@gmail.com>
2025-05-22 10:43:41 +0530

    nested arr & nested obj destructuring

---
+++ BestJSPractices/nestedArrObjDestructing.js
@@ -0,0 +1,27 @@
+// const [num1, num2, [num3, num4, [num5, num6]]] = [1, 2, [3, 4, [5, 6]]];
+// console.log(num1, num2, num3, num4, num5, num6);
+
+// const {
+//     a: num1,
+//     b: num2,
+//     c: {
+//         d: num3,
+//         e: { f: num4, g: num5 },
+//     },
+// } = { a: 1, b: 2, c: { d: 3, e: { f: 4, g: 5 } } };
+
+// console.log(num1, num2, num3, num4, num5);
+
+// const [
+//     num1,
+//     num2,
+//     [
+//         num3,
+//         {
+//             a: num4,
+//             b: [num5, { c: num6 }],
+//         },
+//     ],
+// ] = [1, 2, [3, { a: 4, b: [5, { c: 6 }] }]];
+
+// console.log(num1, num2, num3, num4, num5, num6);
