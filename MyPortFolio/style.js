14de2df118cc2096167c12d8cba9a740cc31a3de
Arpit Jana <arpitjana2103@gmail.com>
2025-08-04 23:18:58 +0530

    Portfolio desktop design done

--- MyPortFolio/style.js
+++ MyPortFolio/style.js
@@ -1,3 +1,5 @@
 // 1. Responsive Header
 // 2. Hide scroll btn if height is less than 745 px
 // 3. Hide scroll btn if user starts scrolling
+// 4. Img Modal Window, img zoom effect
+// 5. Project Screenshot dropdown
